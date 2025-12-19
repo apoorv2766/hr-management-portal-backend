f5ebe4a65dd55f423e4ef1bd0c60d5484da210ca
Apoorv Gupta <145976905+apoorv2766@users.noreply.github.com>
2025-12-19 23:36:37 +0530

    change in login api ,maintain file structure and bug fixes

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -3,7 +3,6 @@ import jwt from "jsonwebtoken";
 const authMiddleware = (req, res, next) => {
   const token = req.headers.authorization?.split(" ")[1];
   if (!token) return res.sendStatus(401);
-
   try {
     req.user = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
     next();
