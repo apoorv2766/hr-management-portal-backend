b2f6699e84257a6d38142d1e270bec34b2c89df4
Apoorv Gupta <145976905+apoorv2766@users.noreply.github.com>
2026-01-15 14:51:38 +0530

    added feature activity tracker api and some bug fixes

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   await mongoose.connect(process.env.MONGO_URI);
-  console.log("database connected");
+  console.log("database is connected");
 };
 
 export default connectDB;
